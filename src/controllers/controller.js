5f900acc080b051ea09b8442c4abc8e851539f11
Suvadeep <maityamalendu18@gmail.com>
2021-11-07 00:08:19 +0530

    added backend with frontend

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 const API_KEY = "61d5d9cbe27f7c52ebaa88cf8dde4cdb";
-const url = `api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
 exports.renderHomePage = (req, res) => {
   res.render("index");
 };
@@ -12,4 +11,16 @@ exports.renderAboutPage = (req, res) => {
 exports.getWeather = (req, res) => {
   const city = req.body.city;
 
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", {
+        weather: `It is currently ${response.data.main.temp} in ${response.data.name}.`,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
